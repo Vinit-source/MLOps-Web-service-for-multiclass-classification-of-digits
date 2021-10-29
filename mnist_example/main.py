87c86ef1f7b8ea243a598b0868e29e6df6a401b8
Vinit-source <vinitgore1234@gmail.com>
2021-10-29 14:41:17 +0530

    Remodularized code and output format

--- mnist_example/main.py
+++ mnist_example/main.py
@@ -55,32 +55,41 @@ digits = datasets.load_digits()
 # vector classifier on the train samples. The fitted classifier can
 # subsequently be used to predict the value of the digit for the samples
 # in the test subset.
+
+# Paths
 CURR_DIR = "."
 MODELS_DIR = f"{CURR_DIR}/mnist_example/models"
+
+# Make model directory if not exists
 os.makedirs(MODELS_DIR) if not osp.exists(MODELS_DIR) else None
-out = pd.DataFrame(columns=["Split", "SVM: Test Accuracy", "Decision Tree: Test Accuracy"])
+# Create output dataframe
+out = pd.DataFrame(columns=["Split", "SVM: Test Accuracy", "Gamma", "Decision Tree: Test Accuracy", "Depth"])
+#!TODO: Convert into command-line program using argparse
+
+# Declare classifier params
+svm_params = {"gamma": [10**i for i in range(-4, 1)]}
+tree_params = {"max_depth": [i for i in range(1,11, 2)]}
+
+# Main loop
 for val_test_ratio in [(0.15, 0.15)]:
     for rescale_factor in [1]:
-        for split in range(5):
-            # preprocess data
-            X, y = preprocess(digits, rescale_factor=rescale_factor)
+        # preprocess data
+        X, y = preprocess(digits, rescale_factor=rescale_factor)
 
+        for split in range(5):
             # split into train, val and test subsets
             X_train, X_val, X_test, y_train, y_val, y_test = create_split(X, y, val_test_ratio=val_test_ratio)
 
-            #!TODO: Convert into command-line program using argparse
             # Create a support vector classifier
-            clf_params = {"gamma": [10**i for i in range(-4, 1)]}
             clf_class = svm.SVC
-            best_clf, max_valid_f1_model, best_hyperparams = run_loop_on_hyperparams(clf_class, clf_params, X_train, y_train, X_val, y_val, val_test_ratio, rescale_factor)
+            best_clf, max_valid_f1_model, best_hyperparams_svm = run_loop_on_hyperparams(clf_class, svm_params, X_train, y_train, X_val, y_val, val_test_ratio, rescale_factor)
 
             # infer on test dataset
             results_svm = test(model=best_clf, X_test=X_test, y_true=y_test)
 
             # Create a Decision tree classifier
-            clf_params = {}
             clf_class = tree.DecisionTreeClassifier
-            best_clf, max_valid_f1_model, best_hyperparams = run_loop_on_hyperparams(clf_class, clf_params, X_train, y_train, X_val, y_val, val_test_ratio, rescale_factor)
+            best_clf, max_valid_f1_model, best_hyperparams_tree = run_loop_on_hyperparams(clf_class, tree_params, X_train, y_train, X_val, y_val, val_test_ratio, rescale_factor)
 
             # infer on test dataset
             results_tree = test(model=best_clf, X_test=X_test, y_true=y_test)
@@ -88,13 +97,16 @@ for val_test_ratio in [(0.15, 0.15)]:
             other = pd.DataFrame({
                 "Split": split+1,
                 "SVM: Test Accuracy": results_svm['acc'], 
-                "Decision Tree: Test Accuracy": results_tree['acc']
+                "Gamma": best_hyperparams_svm["gamma"],
+                "Decision Tree: Test Accuracy": results_tree['acc'],
+                "Depth": best_hyperparams_tree["max_depth"]
                 }, index=[0])
             out = out.append(other, ignore_index=True)
+out = out.round(3)
 stats = pd.DataFrame({
-    "Split":"Stats",
-    "SVM: Test Accuracy": f"{float(out['SVM: Test Accuracy'].mean()):.5f}+/-{float(out['SVM: Test Accuracy'].std()):.5f}", 
-    "Decision Tree: Test Accuracy": f"{float(out['Decision Tree: Test Accuracy'].mean()):.5f}+/-{float(out['Decision Tree: Test Accuracy'].std()):.5f}"
+    "Split":"Mean +/- Std-dev",
+    "SVM: Test Accuracy": f"{float(out['SVM: Test Accuracy'].mean()):.3f}+/-{float(out['SVM: Test Accuracy'].std()):.3f}", 
+    "Decision Tree: Test Accuracy": f"{float(out['Decision Tree: Test Accuracy'].mean()):.3f}+/-{float(out['Decision Tree: Test Accuracy'].std()):.3f}"
     }, index=[0])
 out = out.append(stats, ignore_index=True)
 print(out.to_markdown())
